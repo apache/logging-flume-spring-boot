d61884952f77df836a87586bca0f9bab4a1ffdd9
Piotr P. Karwasz <pkarwasz-github@apache.org>
2026-07-02 18:59:59 +0200

    Apply spotless formatting (#9)
    This applies Spotless formatting before reviewing #4.
    
    Co-authored-by: Spotless <robots@logging.apache.org>

--- flume-spring-boot/src/main/java/org/apache/flume/spring/boot/controller/FlumeMetricsController.java
+++ flume-spring-boot/src/main/java/org/apache/flume/spring/boot/controller/FlumeMetricsController.java
@@ -1,31 +1,29 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache license, Version 2.0
+ * The ASF licenses this file to you under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the license for the specific language governing permissions and
- * limitations under the license.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.flume.spring.boot.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.Map;
-
 import org.apache.flume.instrumentation.util.JMXPollUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 /**
  * Retrieves Flume Metrics.
@@ -34,15 +32,14 @@ import com.google.gson.reflect.TypeToken;
 @ConditionalOnProperty(prefix = "flume", name = "metrics", havingValue = "http")
 public class FlumeMetricsController {
 
-  private final Type mapType = new MapTypeToken().getType();
-  private final Gson gson = new Gson();
+    private final Type mapType = new MapTypeToken().getType();
+    private final Gson gson = new Gson();
 
-  @GetMapping(value = "/metrics", produces = "application/json;charset=utf-8")
-  public String metrics() {
-    Map<String, Map<String, String>> metricsMap = JMXPollUtil.getAllMBeans();
-    return gson.toJson(metricsMap, mapType);
-  }
+    @GetMapping(value = "/metrics", produces = "application/json;charset=utf-8")
+    public String metrics() {
+        Map<String, Map<String, String>> metricsMap = JMXPollUtil.getAllMBeans();
+        return gson.toJson(metricsMap, mapType);
+    }
 
-  private static class MapTypeToken extends TypeToken<Map<String, Map<String, String>>> {
-  }
+    private static class MapTypeToken extends TypeToken<Map<String, Map<String, String>>> {}
 }
